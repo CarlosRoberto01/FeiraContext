544e26cdbcdec95b98e225f8e6583e7f45bba4b2
Carlos Roberto01 <carloos.bezerra@hotmail.com>
2023-07-25 18:06:29 -0300

    Adicionar produto dentro de carrinho

--- reactContext/src/components/Produto/index.js
+++ reactContext/src/components/Produto/index.js
@@ -7,26 +7,34 @@ import { CarrinhoContext } from 'common/context/Carrinho';
 
 function Produto({ nome, foto, id, valor, unidade }) {
 
-  const { carrinho, setCarrinho} = useContext(CarrinhoContext)
+  const { carrinho, setCarrinho } = useContext(CarrinhoContext)
+
+  function adicionarProduto(novoProduto) {
+    const temOProduto = carrinho.some(itemDoCarrinho => itemDoCarrinho.id === id);
+    if (!temOProduto) {
+      novoProduto.quantidade = 1;
+      return setCarrinho(carrinhoAnterior => [...carrinhoAnterior, novoProduto])
+    }
+    setCarrinho(carrinhoAnterior => carrinhoAnterior.map(itemDoCarrinho => {
+      if (itemDoCarrinho.id === novoProduto.id) itemDoCarrinho.quantidade += 1;
+      return itemDoCarrinho;
+    }))
+  }
 
   return (
     <Container>
       <div>
-        <img
-          src={`/assets/${foto}.png`}
-          alt={`foto de ${nome}`}
+        <img src={`/assets/${foto}.png`} alt={`foto de ${nome}`}
         />
         <p>
           {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
         </p>
       </div>
       <div>
-        <IconButton
-          color="secondary"
-        >
+        <IconButton color="secondary">
           <RemoveIcon />
         </IconButton>
-        <IconButton>
+        <IconButton onClick={() => adicionarProduto({ nome, foto, id, valor })}>
           <AddIcon />
         </IconButton>
       </div>
